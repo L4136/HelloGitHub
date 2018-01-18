02addbc1c44ae079355eb62025278709f3c0ba7a
L4136 <35224756+L4136@users.noreply.github.com>
2018-01-18 11:52:17 +0200

    Add files via upload

---
+++ Week2/w3a10.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace w3a10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number :");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number :");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("1: Multiplication");
+            Console.WriteLine("2: Addition");
+            Console.WriteLine("3: Subtraction");
+            Console.WriteLine("4: Division");
+
+            Console.WriteLine("Pick an operation:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1: Console.WriteLine (firstNumber * secondNumber); break;
+                case 2: Console.WriteLine(firstNumber + secondNumber); break;
+                case 3: Console.WriteLine(firstNumber - secondNumber); break;
+                case 4: Console.WriteLine(firstNumber / secondNumber); break;
+                default: Console.WriteLine("That is not a valid operation."); break;
+
+            }
+        }
+    }
+}
---
+++ Week2/w3a6.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace w3a6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comparison;
+            double RandomNumber(double min, double max) 
+            {
+                Random random = new Random(); return random.Next(1, 100);
+            }
+
+            Console.WriteLine("Guess my number! It is between 1 and 100.");
+            double yourNumber = Convert.ToDouble(Console.ReadLine());
+
+            if (random < yourNumber)
+                comparison = "smaller than";
+            else
+            {
+                if (yourNumber == RandomNumber)
+                    comparison = "exactly the same as";
+                else
+                    comparison = "bigger than";
+            }
+            Console.WriteLine("My number is {0} your number.",
+                              comparison);
+            Console.ReadKey();
+        }
+    }
+}
