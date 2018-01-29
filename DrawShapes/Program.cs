9b8b5a13c4e3db69f5db74310832d15e674a8747
L4136 <L4136@student.jamk.fi>
2018-01-29 15:30:16 +0200

    abstrakti-esimerkki

---
+++ DrawShapes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawShapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Shape shape = new Shape();  ei voi tehdä abstraktista luokasta!
+            Shape circle = new Circle();
+            circle.Draw();
+            // create a list to store objects
+            List<Shape> shapes = new List<Shape>();
+            // use polymorphis to store objects
+            shapes.Add(new Circle());
+            shapes.Add(new Square());
+            shapes.Add(new Circle());
+            shapes.Add(new Square());
+
+            Console.WriteLine("Loop through all drawing objects in a list:");
+            foreach (Shape shape in shapes)
+            {
+                shape.Draw(); // "Draw Circle!" "Draw Square!" ...
+            }
+        }
+    }
+}
