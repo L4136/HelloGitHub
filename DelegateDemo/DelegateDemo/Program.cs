886a397644ead255518984d1aa0a7c0e748359c9
L4136 <L4136@student.jamk.fi>
2018-02-12 15:59:05 +0200

    ma 12.2.

---
+++ DelegateDemo/DelegateDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    class DelegaattiDemo
+    {
+        delegate void FormatoiLuku(double luku);
+
+        public static void Main(string[] args)
+        {
+            //kysytään käyttäjältä jokin desimaaliluku
+            Console.WriteLine("Anna desimaaliluku");
+            string syote = Console.ReadLine();
+            double aluku;
+            if (double.TryParse(syote, out aluku))
+            {
+                //kiinnitetään useita metodeja delegaatille
+                FormatoiLuku formatoija = FormatoiValuutaksi;
+                formatoija += FormatoiKolmelleDesimaalille;
+                //kutsutaan kiinnitetyt delegaatit
+                formatoija(aluku);
+
+                formatoija -= FormatoiValuutaksi;
+                formatoija(aluku);
+            }
+        }
+
+        //metodit
+        static void FormatoiValuutaksi(double luku)
+        {
+            Console.WriteLine("Annettu luku valuuttana {0:C}", luku);
+        }
+        static void FormatoiKolmelleDesimaalille(double luku)
+        {
+            Console.WriteLine("Annettu luku kolmen desimaalin tarkkuudella {0:.###}", luku);
+        }
+
+    }
+}
---
+++ LambdaDemo/LambdaDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //four names to List
+            List<string> nimet = new List<string> { "Arska", "Kalle", "Cecilia", "Jack" };
+            string foo = nimet.FirstOrDefault(x => x.StartsWith("Ja"));
+            Console.WriteLine(foo); //this prints Jack
+        }
+
+    }
+}
