6d832db7c7074c5befb1b7aa95d769bb8a0622c9
L4136 <35224756+L4136@users.noreply.github.com>
2018-02-21 21:07:47 +0200

    T3t5 done

---
+++ StudentT3t5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T3t5
+{
+    class Student
+    {
+        public string Firstname { get; set; }
+        public String Lastname { get; set; }
+        public String StudentID { get; set; }
+   
+        public override string ToString()
+        {
+            return "Student:\nFirstname: " + Firstname + " Lastname: " + Lastname + " StudentID: " + StudentID ;
+        }
+
+    }
+}
---
+++ T3t5main.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T3t5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[5];
+            students[0] = new Student { Firstname = "Alli", Lastname = "Alhainen", StudentID = "A1234"};
+            students[1] = new Student { Firstname = "Bertta", Lastname = "Baratheon", StudentID = "B2345"};
+            students[2] = new Student { Firstname = "Carl", Lastname = "Capulet", StudentID = "C3456"};
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine(student.ToString());
+                
+            }
+
+
+        }
+    }
+}
